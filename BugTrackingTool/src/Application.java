95c0c736109ace792f5202c01d889cefa1bf48cb
unknown <ibmjavanh03@iiht.tech>
2021-03-17 22:10:28 +0530

    Added Project class

--- BugTrackingTool/src/Application.java
+++ BugTrackingTool/src/Application.java
@@ -3,13 +3,14 @@ public class Application {
 	
 	public static void main(String[] args) {
 		
+		
 		Bug bug1 = new Bug(BUGSTATUS.OPEN,SEVERITY.HIGH,"1","StackoverFlow error","first error","project1","developer1","tester1");
 		
 		
 		Tester tester = new Tester("Ram");
 		Developer developer = new Developer("Shyam");
 		Manager manager  = new Manager();
-		manager.assign(bug1, tester);
+		
 		//developer.addBug(bug1);
 //		developer.editBugStatus(bug1, BUGSTATUS.VERIFIED, SEVERITY.MEDIUM);
 //		developer.showBug(bug1);
